3de9f3c3250be4a071ecfed1a4f41ba1001428d4
runnz121 <runnz121@gmail.com>
2022-01-13 21:03:40 +0900

    first

---
+++ src/main/java/malangcute/bellytime/bellytimeCustomer/BellytimeCustomerApplication.java
@@ -0,0 +1,13 @@
+package malangcute.bellytime.bellytimeCustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellytimeCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellytimeCustomerApplication.class, args);
+	}
+
+}
